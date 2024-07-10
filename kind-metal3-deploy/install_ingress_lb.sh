6e35beaa38d10bedb9584ea0b4396c4a124fc3ff
Jianzhu Zhang <jianzzha@redhat.com>
2024-07-10 14:49:05 -0400

    Fix test failure when bmh virt-net is not used

--- kind-metal3-deploy/install_ingress_lb.sh
+++ kind-metal3-deploy/install_ingress_lb.sh
@@ -16,5 +16,6 @@ kubectl patch configmap/ingress-nginx-controller -n  ingress-nginx --type merge 
 
 ingress_ip=$(kubectl get svc  -n ingress-nginx ingress-nginx-controller  --no-headers --output=custom-columns=:.status.loadBalancer.ingress[0].ip)
 
-virsh net-update bmh add dns-host "<host ip='${ingress_ip}'><hostname>assisted.com</hostname></host>" --live --config
-
+if virsh net-uuid bmh >/dev/null 2>&1; then
+    virsh net-update bmh add dns-host "<host ip='${ingress_ip}'><hostname>assisted.com</hostname></host>" --live --config
+fi
